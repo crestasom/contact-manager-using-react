89805da79466085806824a7e2e63bb94b6dfa790
crestasom <crestasom@gmail.com>
2020-06-11 20:32:54 +0545

    Updates

---
+++ precache-manifest.3d6ed13e8b61e7a40a26c5d9dd09a81a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a226927629763092d77580d00d8e56",
+    "url": "/contact-manager-using-react/index.html"
+  },
+  {
+    "revision": "45cb5b175cde73a71444",
+    "url": "/contact-manager-using-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "45cb5b175cde73a71444",
+    "url": "/contact-manager-using-react/static/js/2.cdfb701e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact-manager-using-react/static/js/2.cdfb701e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50f4f87bf59e58566e8c",
+    "url": "/contact-manager-using-react/static/js/main.db8e784a.chunk.js"
+  },
+  {
+    "revision": "d6426a12c30dcbcd178e",
+    "url": "/contact-manager-using-react/static/js/runtime-main.f70b53f0.js"
+  }
+]);
